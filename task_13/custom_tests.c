8579bf2907193f521d370e81826c624d36b224c4
Simon Sure <info@simonsure.com>
2023-12-19 16:31:13 +0100

    add custom tests task 13

--- task_13/custom_tests.c
+++ task_13/custom_tests.c
@@ -6,6 +6,28 @@
  */
 #include <stdio.h>
 
+
+// THIS IS NOT PART OF THE ORIGINAL FILE!!!
+#include "ll.h"
+struct node {
+  int value;
+  unsigned int frequency;
+  struct node* next;
+};
+void print_list(struct node* head) {
+  if(head==NULL)
+    printf("EMPTY\n");
+  while(head!=NULL) {
+    if(head->frequency==0) {
+      printf("SHIIIIIIITTTT!!!!\n");
+      return;
+    }
+    printf("%d - %d\n", head->value, head->frequency);
+    head = head->next;
+  }
+}
+
+// THIS HAS BEEN BY ME FOR QUICKER TESTING
 void custom_tests(void)
 {
     /*
@@ -14,4 +36,14 @@ void custom_tests(void)
      */
 
     printf("Running custom tests...\n");
+    struct node* head = insert_value(NULL, 3);
+    head = insert_value(head, 8);
+    head = insert_value(head, 3);
+    head = insert_value(head, 2);
+    head = insert_value(head, 4);
+    head = insert_value(head, 3);
+    head = insert_value(head, 234);
+    head = insert_value(head, 3);
+    head = delete_value(head, 3);
+    print_list(head);
 }
